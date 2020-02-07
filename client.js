0c534117ca476d647a9b2677b76aca8aed9ae118
amir <dev.amirhemmati@yahoo.com>
2020-02-07 20:49:40 +0330

    adding

--- client.js
+++ client.js
@@ -2,24 +2,26 @@
 
         /* Video Chat */
 // Prefer camera resolution nearest to 1280x720.
-var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
+var constraints = { audio: true, video: true }; 
 
 navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
-  const video = document.querySelector("#rVideo");
-  console.log(video);
-  video.srcObject = mediaStream;
-  video.onloadedmetadata = function(e) {
-    video.play();
-  };
-})
-.catch(function(err) { alert("starting Error"); console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+  .then(function(mediaStream) {
+    const video = document.querySelector("#lVideo");
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
 
 
-    // get the local video and display it with permission
+        // get the local video and display it with permission
+    const peer = new Peer({ key: "lwjd5qra8257b9" }); 
 
-    // create a peer connection with peer obj
-
+        // create a peer connection with peer obj
+    peer.on('open', () => {
+      document.getElementById("displayId").innerHTML = peer.id;
+    })
+  })
+  .catch(function(err) { alert("starting Error"); console.log(err.name + ": " + err.message); }); // always check for errors at the end.
     // display the peer id on the DOM
 
     // onclick with the connection butt = exposes ice info
